7e79252a248a3cee5d30859e26f9b969133389d6
Dont33 <viper772@yandex.ru>
2015-02-06 18:13:07 +0400

    After change routes

--- spec/requests/static_apges_spec.rb
+++ spec/requests/static_apges_spec.rb
@@ -6,37 +6,37 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{static_title}")
     end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title(' | Home')
     end
   end
  describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the the title Help" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{static_title} | Help")
     end
   end
 describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the the title About Us" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{static_title} | About Us")
     end
   end
@@ -44,11 +44,11 @@ describe "About page" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the the title Contact" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{static_title} | Contact")
     end
   end
